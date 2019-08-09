4be011b3efc9e42a1847d356619d7ad19598d6d6
nshaffer26 <nshaffer26@outlook.com>
2019-08-09 16:55:48 -0400

    Port change test 2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
-const port = 8080;
+const port = 3306;
 
 const bodyParser = require('body-parser');
 
